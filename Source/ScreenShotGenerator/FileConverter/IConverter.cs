bcf88ccfe50aa3996296355549027f94a3894bec
Bob <7878459RT@tt.ru>
2023-04-29 17:21:28 +0300

    Создание нового тестового приложения. Перенос кода.

---
+++ Source/ScreenShotGenerator/FileConverter/IConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConverter
+{
+    internal interface IConverter
+    {
+        /// <summary>
+        /// Преобразовывает файл из одного вида в другой. 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="outFileName"></param>
+        /// <param name="printInfo"></param>
+        /// <param name="parcer"></param>
+        void ConvertFile(string fileName, string outFileName, Action<string> printInfo,
+            Func<List<string>, string, bool> parcer);
+    }
+}
---
+++ Source/ScreenShotGenerator/TestService/Models/RetJson.cs
@@ -0,0 +1,29 @@
+﻿namespace TestService
+{
+    /// <summary>
+    /// Модель возвращаемого json.
+    /// </summary>
+    internal class RetJson
+    {
+        /// <summary>
+        /// Путь к сайту.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Результат работы скриншоттера.
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// Путь к локальной папке.
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Сообщение -в случае ошибки, и информационные сообщения.
+        /// </summary>
+        public string Log { get; set; }
+
+    }
+}
